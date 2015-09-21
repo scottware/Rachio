1def9a6563fbef38156d353ba0196183ca7ff16d
Scott Ware <sware@mac.com>
2015-09-20 23:05:15 -0700

    first checkin

---
+++ src/main/java/com/scott/app/Rachio/Rachio.java
@@ -0,0 +1,24 @@
+package com.scott.app.Rachio;
+
+import org.json.simple.parser.ParseException;
+
+public class Rachio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		API api = new API();
+		// api.setPersonID();
+		// api.fetchPerson();
+		api.testPrint();
+		// System.out.println(api.getPersonId());
+		//
+		// String jsonText =
+		// "{\"id\":\"a28a5069-afdf-45b1-a497-c0070881e84d\"}";
+		// try {
+		// api.parseText(jsonText);
+		// } catch (ParseException e) {
+		// e.printStackTrace();
+		// }
+	}
+
+}
